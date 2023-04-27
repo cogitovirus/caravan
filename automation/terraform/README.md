90a5f6de601550d7a6a68aa1894e46bef38361ce
wzolni <w.zolnierowicz@gmail.com>
2023-04-27 18:45:11 +0200

    terraform for nifi

---
+++ automation/terraform/README.md
@@ -0,0 +1,18 @@
+# Terraform / NiFi on Google Cloud
+
+Clone of https://github.com/pvillard31/nifi-gcp-terraform
+
+This repository is used to deploy NiFi instances using Terraform on the Google Cloud Platform.
+
+* [gcp-single-secured-nifi-oidc](./gcp-single-secured-nifi-oidc) - Will:
+  * deploy a NiFi CA server as a convenient way to generate SSL certificates
+  * deploy a single secured NiFi instance mapped to a domain
+  * configure NiFi to use OpenID connect for authentication
+* [gcp-cluster-secured-nifi-oidc](./gcp-cluster-secured-nifi-oidc) - Will:
+  * deploy a NiFi CA server as a convenient way to generate SSL certificates
+  * deploy an external ZooKeeper instance to manage cluster coordination and state across the nodes
+  * deploy one secured instance of the NiFi Registry configured with OIDC
+  * deploy X secured NiFi instances clustered together
+  * configure NiFi to use OpenID connect for authentication
+  * configure an HTTPS load balancer with Client IP affinity in front of the NiFi cluster
+
---
+++ automation/terraform/gcp-cluster-secured-nifi-oidc/variables.tf
@@ -0,0 +1,109 @@
+variable network_name {
+    default = "nifi-network"
+}
+
+variable nifi-ca-hostname {
+    default = "nifi-ca"
+}
+
+variable zookeeper-hostname {
+    default = "zookeeper"
+}
+
+variable registry-hostname {
+    default = "nifi-registry"
+}
+
+variable nifi-hostname {
+    default = "nifi"
+}
+
+variable nifi-basedir {
+    default = "/opt/nifi"
+}
+
+variable nifi-machine-type {
+    default = "e2-highcpu-4"
+}
+
+variable nifi-ca-machine-type {
+    default = "e2-micro"
+}
+
+variable zookeeper-machine-type {
+    default = "e2-micro"
+}
+
+variable registry-machine-type {
+    default = "e2-micro"
+}
+
+variable zookeeper_version {
+    default = "3.6.3"
+}
+
+variable nifi_version {
+    default = "1.15.0"
+}
+
+variable nifiregistry_version {
+    default = "1.15.0"
+}
+
+variable nifi_toolkit_version {
+    default = "1.15.0"
+}
+
+variable "region" {
+    default = "europe-west1"
+}
+
+variable "zone" {
+    default = "europe-west1-d"
+}
+
+// ---------------------------------
+
+variable "project" {
+    description = "GCP Project ID"
+}
+
+variable nifi-admin {
+    description = "Google mail address for the user that will be the initial admin in NiFi"
+}
+
+variable san {
+    description = "FQDN of the DNS mapping that will be used to access NiFi. Example: nifi.example.com"
+}
+
+variable san-registry {
+    description = "FQDN of the DNS mapping that will be used to access NiFi Registry. Example: nifiregistry.example.com"
+}
+
+variable proxyhost {
+    description = "FQDN:port that will be used to access NiFi. Example: nifi.example.com:8443"
+}
+
+variable ca_token {
+    description = "The token to use to prevent MITM between the NiFi CA client and the NiFi CA server (must be at least 16 bytes long)"
+}
+
+variable oauth_clientid {
+    description = "OAuth Client ID"
+}
+
+variable oauth_secret {
+    description = "OAuth Client secret"
+}
+
+variable "instance_count" {
+    description = "Number of NiFi instances"
+}
+
+variable nifi_bucket {
+    description = "GCS path to the bucket containing the binaries (ex: gs://nifi_bin)"
+}
+
+variable sensitivepropskey {
+    description = "Key that will be used for encrypting the sensitive properties in the flow definition (ex: ThisIsAVeryBadPass3word)"
+}
